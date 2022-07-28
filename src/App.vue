6aaebdb111f66452f614fc93c3991e8fec42ec60
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-28 15:02:46 +0200

    creata url dinamica, ma con valore stringa vuota da errore

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,26 @@ import axios from "axios"
 
 export default {
   name: 'App',
+  
+  components: {},
+  created(){
+    axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.queryValue}`)
+    .then(response => {
+      this.movies = response.data.results}
+
+      
+      
+    )
+  },
   data(){
     return {
       baseUri: "https://api.themoviedb.org/3",
       apiKey: "1c5e26a324a176f1263f8bbddd9a8b1f",
-      queryValue: "anelli"
+      queryValue: "",
+      movies: [],
     }
 
   },
-  components: {},
-  created(){
-    axios.get("baseUri/search/movie?api_key=apiKey&query=anelli")
-  }
 }
 </script>
 

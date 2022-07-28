5954d383ddfd95d332e1c394255f45d529d7187f
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-28 16:18:01 +0200

    stampa 10 oggetti movie

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,50 @@
 <template>
   <div id="app">
+    <input type="text" v-model="searched" :key="movies.id"/> 
     
+    <button @click="getMovie">
+    </button>
+
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">{{movie}}</li>
+    </ul>
+
     
   </div>
 </template>
 
 <script>
-import axios from "axios"
-
+import axios from "axios";
 
 export default {
-  name: 'App',
-  
-  components: {},
-  created(){
-    axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.queryValue}`)
-    .then(response => {
-      this.movies = response.data.results}
+  name: "App",
 
-      
-      
-    )
-  },
-  data(){
+  data() {
     return {
       baseUri: "https://api.themoviedb.org/3",
       apiKey: "1c5e26a324a176f1263f8bbddd9a8b1f",
-      queryValue: "",
+      searched: "",
       movies: [],
-    }
-
+    };
   },
-}
+
+  components: {},
+
+  methods: {
+    getMovie(){
+    axios
+      .get(
+        `${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.searched}`
+      )
+      .then((response) => {
+        this.movies = response.data.results;
+      });
+    }
+    
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  
-}
+
 </style>

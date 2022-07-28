dbc7c2962258c1548526f85aa2a675ca81462cd6
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-28 16:25:02 +0200

    stampa anche serie tv

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <div id="app">
     <input type="text" v-model="searched" :key="movies.id"/> 
     
-    <button @click="getMovie">
+    <button @click="getMovies(); getTvSeries();">
     </button>
 
-    <ul>
+    <ul id="films">
       <li v-for="movie in movies" :key="movie.id">{{movie}}</li>
+    </ul>
+
+    <ul id="serie-tv">
+      <li v-for="serie in series" :key="serie.id">{{serie}}</li>
     </ul>
 
     
@@ -25,19 +29,30 @@ export default {
       apiKey: "1c5e26a324a176f1263f8bbddd9a8b1f",
       searched: "",
       movies: [],
+      series: []
     };
   },
 
   components: {},
 
   methods: {
-    getMovie(){
+    getMovies(){
     axios
       .get(
         `${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.searched}`
       )
       .then((response) => {
         this.movies = response.data.results;
+      });
+    },
+
+    getTvSeries(){
+    axios
+      .get(
+        `${this.baseUri}/search/tv?api_key=${this.apiKey}&query=${this.searched}`
+      )
+      .then((response) => {
+        this.tvSeries = response.data.results;
       });
     }
     
